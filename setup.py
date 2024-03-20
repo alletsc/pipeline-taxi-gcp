cd9be8448324188100555571ae03e6369bf771cc
S. Costa <70285709+alletsc@users.noreply.github.com>
2024-03-20 16:19:10 -0300

    Set up project initial organization and metadata #1 (#15)
    closes #1

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='taxi-gcp',
+    version='0.1.0',
+    description='Pipeline for taxi data on GCP',
+    author='Stella Costa',
+    packages=find_packages()
+)
