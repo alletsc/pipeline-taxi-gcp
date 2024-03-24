6442c26963894853a4dfa25ad84e33035f1ae04e
Stella <stellamaryane@gmail.com>
2024-03-24 18:29:19 -0300

    Added CI GHA #3

--- setup.py
+++ setup.py
@@ -10,31 +10,29 @@ def read(*paths):
     with open(filepath) as file_:
         return file_.read().strip()
 
+
 def read_requirements(path):
     """Lê as dependências do projeto a partir de um arquivo .txt."""
     return [
         line.strip()
         for line in read(path).split("\n")
-        if not line.startswith(("#", "git+", '"', '-'))
+        if not line.startswith(("#", "git+", '"', "-"))
     ]
 
 
 setup(
-    name='taxi_gcp',
-    version='0.1.0',
-    description='Pipeline for taxi data on GCP',
+    name="taxi_gcp",
+    version="0.1.0",
+    description="Pipeline for taxi data on GCP",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
-    author='Stella Costa',
+    author="Stella Costa",
+    python_requires=">=3.9",
     packages=find_packages(),
-    entry_points={
-        "console_scripts": [
-            "taxi_gcp=taxi_gcp.__main__:main"
-        ]
-    },
+    entry_points={"console_scripts": ["taxi_gcp=taxi_gcp.__main__:main"]},
     install_requires=read_requirements("requirements.txt"),
     extras_require={
         "test": read_requirements("requirements-test.txt"),
-        "dev": read_requirements("requirements-dev.txt")
-    }
+        "dev": read_requirements("requirements-dev.txt"),
+    },
 )
